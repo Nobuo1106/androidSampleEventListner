62fee6df161dfa73ada02d001017535e068569a9
Nobuo1106 <gonnbe1986@gmail.com>
2023-07-24 22:08:57 +0900

    表示ボタン追加

--- app/src/main/java/com/websarva/wings/android/hellosample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/hellosample/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.websarva.wings.android.hellosample
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,7 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btClick = findViewById<Button>(R.id.btClick)
+        val listener = HelloListner()
+        btClick.setOnClickListener(listener)
     }
 
-    
+    private inner class HelloListner : View.OnClickListener {
+        override  fun onClick(view: View) {
+            val input = findViewById<EditText>(R.id.etName)
+            val output = findViewById<TextView>(R.id.tvOutput)
+            val inputstr = input.text.toString()
+            output.text = inputstr + "さん、こんにちは"
+        }
+    }
 }
